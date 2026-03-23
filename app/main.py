5a99a5ee5ad5e8da305c87de866bf71f4a709836
shafran <shafran.andriy@gmail.com>
2026-03-23 12:28:55 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -57,9 +57,12 @@ class Dictionary:
                         else:
                             # If we have collision
                             index = random.choice(
-                                [index for index in range(len(temp_list))
-                                 if temp_list[index] is None]
-                                )
+                                [
+                                    index
+                                    for index in range(len(temp_list))
+                                    if temp_list[index] is None
+                                ]
+                            )
                             temp_list[index] = value
                             self._dict[key] = temp_list[index]
                 self.hash_table = temp_list
